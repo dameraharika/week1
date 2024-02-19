1a45fd5e4b66cac0907a7a4705f9c680ac4475e2
dameraharika <147978157+dameraharika@users.noreply.github.com>
2024-02-19 18:25:24 +0530

    Delete week12.R

--- week12.R
+++
@@ -1,8 +0,0 @@
-student_name <- c("harika", "poojitha", "siri")
-name <- c(18, 19, 20)
-weight <- c(50, 30, 45)
-height <- c(100, 150, 250)
-gender <- c("Male", "Female", "Male")
-
-data <- data.frame(student_name, name, weight, height, gender)
-print(data)
